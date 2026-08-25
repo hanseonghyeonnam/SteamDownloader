60ac2e47623202a5c16bfa40dbdf617565e87599
hanseonghyeonnam <hanseonghyeonnam@gmail.com>
2026-08-26 01:04:53 +0900

    done

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"github.com/pkg/browser"
 	"os"
+	"fmt"
 )
 
 func main() {
+	fmt.Println("[This is a security-awareness demo, not a game unlocker.]")
 	browser.OpenURL("https://pastebin.com/eQETzdvW")
 	os.Exit(1)
 }
